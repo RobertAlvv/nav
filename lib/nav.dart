542c839a2ed067b41c80c3784516b1eea7260c97
Robert Alvarez <alvarezrobert150@gmail.com>
2022-06-01 09:57:30 -0400

    add route push Widget

--- lib/nav.dart
+++ lib/nav.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 
 class Nav {
   static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
-  static navigatorPush(String route) {
+
+  static navigatorPushName(String route) {
     Nav.navigatorKey.currentState!.pushNamed(route);
+  }
+
+  static navigatorPush(Widget page) {
+    Nav.navigatorKey.currentState!.push(
+      MaterialPageRoute(builder: (context) => page),
+    );
   }
 
   static navigatorPop() {
@@ -16,5 +23,16 @@ class Nav {
 
   static navigatorPopAndPush(String route) {
     Nav.navigatorKey.currentState!.popAndPushNamed(route);
+  }
+
+  static navigatorPushByAnimation(Widget Function() view) {
+    Nav.navigatorKey.currentState!.push(
+      PageRouteBuilder(
+        pageBuilder: (c, a1, a2) => view(),
+        transitionsBuilder: (c, anim, a2, child) =>
+            FadeTransition(opacity: anim, child: child),
+        transitionDuration: Duration(milliseconds: 2000),
+      ),
+    );
   }
 }
