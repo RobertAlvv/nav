5263d0e4ae6649f03a25f2c570f15cbae9104971
Robert Alvarez <alvarezrobert150@gmail.com>
2022-06-02 13:48:44 -0400

    add context with modal

--- lib/draggable.dart
+++ lib/draggable.dart
@@ -2,16 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:nav/nav.dart';
 
 class CustomDraggable {
-  static showBottomSheet(Widget content,
-          [Color color = Colors.white,
-          bool isScrollControlled = true,
-          bool isDismissible = true]) =>
+  static showBottomSheet({
+    required Widget content,
+    required BuildContext context,
+    Color color = Colors.white,
+    bool isScrollControlled = true,
+    bool isDismissible = true,
+  }) =>
       showModalBottomSheet(
-        context: Nav.navigatorKey.currentState!.context,
+        context: context,
         isScrollControlled: isScrollControlled,
         isDismissible: isDismissible,
         shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(20),
+          ),
+        ),
         backgroundColor: color,
         builder: (context) => content,
       );
